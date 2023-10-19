d94ff7b74af01167c4970162954510fc5e9e4682
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2023-10-19 14:44:59 +0000

    'Creating new service p800-refunds-stubs'

---
+++ app/uk/gov/hmrc/p800refundsstubs/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.p800refundsstubs.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
