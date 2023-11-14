1500fc916aace8c5bf762bdc9053ee64f78b0da6
Myles Offord <2564924+Lyrain@users.noreply.github.com>
2023-11-14 10:54:32 +0000

    [MO][OPS-11115] Use ItSpec & RichMatchers for tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,11 +10,15 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion,
+    "org.typelevel" %% "cats-core" % "2.10.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % "test, it",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % Test,
+    "org.scalatest" %% "scalatest" % "3.2.17" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
   )
 }
---
+++ test/uk/gov/hmrc/p800refundsstubs/testsupport/ItSpec.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.p800refundsstubs.testsupport
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.freespec.AnyFreeSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.{DefaultTestServerFactory, RunningServer}
+import play.api.{Application, Mode}
+import play.core.server.ServerConfig
+import testsupport.RichMatchers
+import testsupport.wiremock.WireMockSupport
+
+trait ItSpec extends AnyFreeSpecLike
+  with RichMatchers
+  with BeforeAndAfterEach
+  with GuiceOneServerPerSuite
+  with WireMockSupport
+  with Matchers {
+
+  private val testServerPort = 19001
+  private val databaseName: String = "p800-refunds-frontend-it"
+  lazy val webdriverUrl: String = s"http://localhost:${port.toString}"
+
+  protected implicit val webDriver: WebDriver = new HtmlUnitDriver()
+
+  protected lazy val configMap: Map[String, Any] = Map[String, Any](
+    "mongodb.uri" -> s"mongodb://localhost:27017/$databaseName",
+    "play.http.router" -> "testOnlyDoNotUseInAppConf.Routes",
+    "auditing.consumer.baseUri.port" -> WireMockSupport.port,
+    "auditing.enabled" -> false,
+    "auditing.traceRequests" -> false
+  )
+
+  override def fakeApplication(): Application = new GuiceApplicationBuilder()
+    .configure(configMap).build()
+
+  override implicit protected lazy val runningServer: RunningServer =
+    TestServerFactory.start(app)
+
+  object TestServerFactory extends DefaultTestServerFactory {
+    override protected def serverConfig(app: Application): ServerConfig = {
+      val sc = ServerConfig(port    = Some(testServerPort), sslPort = Some(0), mode = Mode.Test, rootDir = app.path)
+      sc.copy(configuration = sc.configuration.withFallback(overrideServerConfiguration(app)))
+    }
+  }
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    webDriver.manage().deleteAllCookies()
+  }
+
+  def goToViaPath(path: String): Unit = webDriver.get(s"$webdriverUrl$path")
+
+}
