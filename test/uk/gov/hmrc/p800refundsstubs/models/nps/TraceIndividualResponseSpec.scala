b109698110e483f0e45d0b958b28febcf017fb77
Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>
2024-02-21 13:59:10 +0000

    [PP][ops-11607] test for serialising json
    Signed-off-by: Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>

---
+++ test/uk/gov/hmrc/p800refundsstubs/models/nps/TraceIndividualResponseSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.p800refundsstubs.models.nps
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.p800refundsstubs.models.Nino
+import uk.gov.hmrc.p800refundsstubs.testsupport.UnitSpec
+
+class TraceIndividualResponseSpec extends UnitSpec {
+
+  "serialize" in {
+
+    val t = TraceIndividualResponse(
+      identifier = Nino("AB999999C")
+    )
+
+    Json.toJson(t) shouldBe Json.parse(
+      //Language=JSON
+      """{
+        "identifier" : "AB999999C",
+        "firstForename" : "Bob",
+        "surname" : "Ross",
+        "dateOfBirth" : "1978-01-02",
+        "addressType" : "RESIDENTIAL",
+        "addressLine1" : "Flat 1 Rose House",
+        "addressLine2" : "Worthing",
+        "addressPostcode" : "BN12 4XL",
+        "country" : "ENGLAND"
+      }"""
+    )
+  }
+
+}
