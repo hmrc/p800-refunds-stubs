6d208f58f1b46da949a590ae1768324fcdd5ab55
Myles Offord <2564924+Lyrain@users.noreply.github.com>
2024-05-20 17:13:30 +0100

    [MO][OPS-9748] Update README. Remove unused functionality.

--- test/uk/gov/hmrc/p800refundsstubs/controllers/EcospendControllerSpec.scala
+++ test/uk/gov/hmrc/p800refundsstubs/controllers/EcospendControllerSpec.scala
@@ -16,12 +16,8 @@
 
 package uk.gov.hmrc.p800refundsstubs.controllers
 
-import play.api.http.HeaderNames.CONTENT_TYPE
 import play.api.libs.json._
-import play.api.test.FakeRequest
-import play.api.test.Helpers._
 import uk.gov.hmrc.p800refundsstubs.models._
-import uk.gov.hmrc.p800refundsstubs.models.bankverification._
 import uk.gov.hmrc.p800refundsstubs.testsupport.ItSpec
 
 import java.util.{Currency, UUID}
@@ -29,55 +25,6 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class EcospendControllerSpec extends ItSpec {
-
-  private val ecospendController = app.injector.instanceOf[EcospendController]
-
-  private def fakeRequest(identifier: String): FakeRequest[BankVerificationRequest] = FakeRequest()
-    .withBody(BankVerificationRequest(identifier))
-    .withHeaders(CONTENT_TYPE -> JSON, AUTHORIZATION -> "Bearer 1234567890")
-
-  private def prepMongo(identifier: String, verificationStatus: VerificationStatus) =
-    bankVerificationRepo
-      .insert(BankVerificationEntry(java.time.Instant.now(), BankVerification(identifier, verificationStatus)))
-      .futureValue
-
-  "POST /notification" - {
-    "return 402 (PaymentRequired) if no entry in mongo" in {
-      val result = ecospendController.notification(fakeRequest("AB123456A"))
-      status(result) shouldBe PAYMENT_REQUIRED
-    }
-
-    "return Successful when that is the value in mongo" in {
-      val identifier: String = "LM001014C"
-      prepMongo(identifier, VerificationStatus.Successful)
-      val result = ecospendController.notification(fakeRequest(identifier))
-      status(result) shouldBe 200
-      //language=JSON
-      val expectedJsonResponse = """{"identifier":"LM001014C","verificationStatus":"Successful"}"""
-      contentAsString(result) shouldBe expectedJsonResponse
-    }
-
-    "return UnSuccessful when that is the value in mongo" in {
-      val identifier: String = "MA000003B"
-      prepMongo(identifier, VerificationStatus.UnSuccessful)
-      val result = ecospendController.notification(fakeRequest(identifier))
-      status(result) shouldBe 200
-      //language=JSON
-      val expectedJsonResponse = """{"identifier":"MA000003B","verificationStatus":"UnSuccessful"}"""
-      contentAsString(result) shouldBe expectedJsonResponse
-    }
-
-    "update to a status when called a second time by the frontend" in {
-      val identifier: String = "AB123456C"
-      val firstCall = ecospendController.notification(fakeRequest(identifier))
-      status(firstCall) shouldBe PAYMENT_REQUIRED
-      val secondCall = ecospendController.notification(fakeRequest(identifier))
-      status(secondCall) shouldBe 200
-      //language=JSON
-      val expectedJsonResponse = """{"identifier":"AB123456C","verificationStatus":"Successful"}"""
-      contentAsString(secondCall) shouldBe expectedJsonResponse
-    }
-  }
 
   "Validate custom JSON parsing for BankAccountSummaryResponse" in {
     //language=JSON
